467e3e6adc39b8e95cc9baa48ef11eeb5430ac1b
Pushkar Alange <pushkarbalange14@gmail.com>
2026-08-28 08:41:41 +0530

    Add NEW.c with a simple boolean check

---
+++ NEW.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    bool active = true;
+    int number = 0b1010;
+
+    if (active)
+        printf("Number = %d\n", number);
+
+    return 0;
+}
